42c346c25a422244db9d94dba519f75264cb4cb8
truongnh <truongnh.itvn@gmail.com>
2020-06-14 23:15:32 +0700

    init embedded keycloak

---
+++ lsso-authorization-server/src/main/java/com/truongnh/lsso/LssoAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.truongnh.lsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LssoAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LssoAuthorizationServerApplication.class, args);
+	}
+
+}
